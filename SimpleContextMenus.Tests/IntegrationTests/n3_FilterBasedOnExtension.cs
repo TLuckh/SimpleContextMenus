9d93e474773d7a8cb6230fb5b81c918e97916c25
TLuckH <Luckhard@students.uni-marburg.de>
2026-06-25 06:21:42 +0200

    Force sequentially of tests. Only n3 done for now

--- SimpleContextMenus.Tests/IntegrationTests/n3_FilterBasedOnExtension.cs
+++ SimpleContextMenus.Tests/IntegrationTests/n3_FilterBasedOnExtension.cs
@@ -12,8 +12,26 @@ public class FilterBasedOnExtension : IntegrationTestBaseClass
     {
         var fullTestFolderPath = Path.Combine(PathToAssemblyFolder, TestFolderPath);
         if (Directory.Exists(fullTestFolderPath))
+        {
+            CheckFileLocks(fullTestFolderPath);
+
+
             Directory.Delete(fullTestFolderPath, true);
+        }
+
         Directory.CreateDirectory(fullTestFolderPath);
+        return;
+
+
+        void CheckFileLocks(string s)
+        {
+            foreach (string? file in Directory.EnumerateFiles(s, "*", SearchOption.AllDirectories))
+            {
+                var x = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.None, 4096,
+                    FileOptions.Asynchronous);
+                x.Dispose();
+            }
+        }
     }
 
     [Fact]
@@ -92,7 +110,7 @@ public class FilterBasedOnExtension : IntegrationTestBaseClass
     [Fact]
     public void FilterOnMimeFineType_VIDEONotFilteredOut()
     {
-         string testFolderPath = TestFolderPath;
+        string testFolderPath = TestFolderPath;
         List<string> selectedTestItems = ["Dummy.mp3", "Dummy1.mp4", "Dummy2.mp4"];
         CopyFilesFromIntegrationTestsToAssemblyFolder(
             newFolderToCopyInto: testFolderPath,
@@ -121,36 +139,38 @@ public class FilterBasedOnExtension : IntegrationTestBaseClass
     [Fact]
     public void FilterOnMime_VIDEOFiltersSelectedFiles()
     {
-         string testFolderPath = TestFolderPath;;
+        string testFolderPath = TestFolderPath;
+        ;
         List<string> selectedTestItems = ["Dummy.mp3", "Dummy1.mp4", "Dummy2.mp4", "Dummy.mkv"];
         CopyFilesFromIntegrationTestsToAssemblyFolder(
             newFolderToCopyInto: testFolderPath,
             selectedTestItems.ToArray());
-        CopyFilesFromIntegrationTestsToTopLevelItems("Only_VIDEO.VIDEO.py","WriteSelectedFilesResult.txt");
+        CopyFilesFromIntegrationTestsToTopLevelItems("Only_VIDEO.VIDEO.py", "WriteSelectedFilesResult.txt");
 
         var contextMenuMock = new ContextMenuMock(testFolderPath, selectedTestItems);
         Node? contextMenuEntry = contextMenuMock.contextMenuTree.GetChildByName("Only_VIDEO");
         Assert.NotNull(contextMenuEntry);
-        
+
         contextMenuEntry.Invoke(contextMenuMock.contextMenuInterface);
         Thread.Sleep(1000);
-        
+
         string content;
         var fs = new FileStream(
             Path.Combine(PathToTopLevelItems_Folder, "WriteSelectedFilesResult.txt"),
             FileMode.Open,
             FileAccess.Read,
-            FileShare.None);  // ← We lock the same file
+            FileShare.None); // ← We lock the same file
 
         List<string> selectedTestItemsFiltered = ["Dummy1.mp4", "Dummy2.mp4", "Dummy.mkv"];
 
         using (var file = new StreamReader(fs))
         {
-            Assert.Equal(file.ReadLine().Trim() , Path.Combine(PathToAssemblyFolder,testFolderPath,selectedTestItemsFiltered[0]).Trim());
-            Assert.Equal(file.ReadLine().Trim() , Path.Combine(PathToAssemblyFolder,testFolderPath,selectedTestItemsFiltered[1]).Trim());
-            Assert.Equal(file.ReadLine().Trim() , Path.Combine(PathToAssemblyFolder,testFolderPath,selectedTestItemsFiltered[2]).Trim());
-            
+            Assert.Equal(file.ReadLine().Trim(),
+                Path.Combine(PathToAssemblyFolder, testFolderPath, selectedTestItemsFiltered[0]).Trim());
+            Assert.Equal(file.ReadLine().Trim(),
+                Path.Combine(PathToAssemblyFolder, testFolderPath, selectedTestItemsFiltered[1]).Trim());
+            Assert.Equal(file.ReadLine().Trim(),
+                Path.Combine(PathToAssemblyFolder, testFolderPath, selectedTestItemsFiltered[2]).Trim());
         }
-
     }
 }
